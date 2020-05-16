f7f2d73ce7a8b77d95c1a01c938394a8e543db75
BAIYUNPENG94 <baiyunpeng@yahoo.com>
2020-05-16 17:41:49 +0900

    fix

--- personal_web/src/components/Homepage.vue
+++ personal_web/src/components/Homepage.vue
@@ -11,7 +11,7 @@
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
             <b-nav-item href="/#/Resume">Resume</b-nav-item>
-            <b-nav-item href="/blog">Blog</b-nav-item>
+            <b-nav-item href="https://baiyunpeng.net/">Blog</b-nav-item>
             <b-nav-item href="/#/About">About</b-nav-item>
           </b-navbar-nav>
 
